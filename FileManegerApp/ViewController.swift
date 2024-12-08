33c314b33d4f1508fea21e6a82069749d224e576
aydinnkaya <Aydinnkaya1@gmail.com>
2024-12-08 15:58:35 +0300

    Deploy

--- FileManegerApp/ViewController.swift
+++ FileManegerApp/ViewController.swift
@@ -7,15 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     var pictures = [String]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let fm = FileManager.default
+        
+        let fm = FileManager.default  // Default fm object
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path)
+        let items = try! fm.contentsOfDirectory(atPath: path) // Constant value Arrays(Collections)
         
         
         for item in items {
@@ -23,11 +23,26 @@ class ViewController: UIViewController {
                 pictures.append(item)
             }
         }
-
-        // hi
-        print(pictures)
+                
     }
+    
+    // table view that triggers the request
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = pictures[indexPath.row]
+        cell.contentConfiguration = content
+        //tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
 
-
+        // cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
 }
 
